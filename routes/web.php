47662c9a3c5207008e07ae148d9d3eb19ee85dfe
Michel Tsilavo <micheltsilavodev@protonmail.com>
2024-05-29 01:19:20 -0300

    Born Loggy

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show'])->whereNumber('id')->name('posts.show');
+Route::get('/about', function () {
+    return view('about');
 });
